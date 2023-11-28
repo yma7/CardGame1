7c266b00231fa91a8c93e6eb84825d2a48b5594c
yuanyema <yuanye.ma@menloschool.org>
2023-11-27 21:32:04 -0800

    Added instance variables suit and rank. Added constructor, gettersm and setters for the card class. Added new classes Deck Player and game. In Deck Class added constructor, isEmpty and deal methods.

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,40 @@
 public class Card {
     private String suit;
-    private int rank;
-    private String te;
+    private String rank;
+    private int point;
 
 
-    public Card()
+    public Card(String s, String r, int p)
     {
-        rank = 5;
-        suit = "Hearts";
+        suit = s;
+        rank = r;
+        point = p;
     }
-
+    public String getSuit()
+    {
+        return suit;
+    }
+    public void setSuit(String s)
+    {
+        suit = s;
+    }
+    public String getRank()
+    {
+        return rank;
+    }
+    public void setRank(String r)
+    {
+        rank = r;
+    }
+    public int getPoint()
+    {
+        return point;
+    }
+    public void setPoint(int p)
+    {
+        point = p;
+    }
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
---
+++ src/Deck.java
@@ -0,0 +1,30 @@
+public class Deck {
+    private ArrayList<String> cards;
+    private static int cardsLeft;
+
+    public Deck(String[] ranks, String[] suits, int[] values)
+    {
+        cards = new Arraylist<>();
+        for (String suit: suits)
+        {
+            for (int i = 0; i < ranks.length; i++)
+            {
+                Card card = new Card(suit, rank[i], value[i]);
+                cards.add(card);
+            }
+        }
+    }
+    public boolean isEmpty()
+    {
+        return cardsLeft == 0;
+    }
+
+    public Card deal()
+    {
+        if (cards.isEmpty())
+        {
+            return null;
+        }
+        return cards.get(cardsLeft--);
+    }
+}
